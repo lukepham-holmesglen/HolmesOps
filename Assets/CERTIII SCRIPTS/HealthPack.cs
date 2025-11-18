b7ada1b20747e88f0845ec65d9688bf90cedd694
eongirl03 <epantelopou@student.holmesglen.edu.au>
2025-11-18 11:26:39 +1100

    Healthpack

---
+++ Assets/CERTIII SCRIPTS/HealthPack.cs
@@ -0,0 +1,39 @@
+using System.Data;
+using UnityEngine;
+
+public class HealthPack : MonoBehaviour
+{
+    public int healAmount = 50;
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 
+    /// Variable for the amount of health to add
+    /// set up an OnTriggerEnter function
+    /// check if the collider has the "Player" tag
+    /// if it does access:
+    /// collider.gameObject.GetComponent<Character>().ChangeCurrentHealth(yourAmount);
+    /// make sure to delete the pack on pickup.
+    /// 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<Character>().ChangeCurrentHealth(healAmount);
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
